076a81f40ae5a233795f397e9fdc21157fe851d6
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-10-08 11:34:11 +0530

    Create apktool-termux-official.sh

---
+++ apktool/apktool-termux-official.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L -o $PREFIX/bin/apktool https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
+curl -L -o $PREFIX/bin/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.6.1.jar
+chmod +x $PREFIX/bin/apktool*  
