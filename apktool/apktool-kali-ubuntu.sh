2209759d6ba43b3c30baf9c4651e8e99106b5cce
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2023-03-07 22:40:19 +0530

    Update apktool-kali-ubuntu.sh

--- apktool/apktool-kali-ubuntu.sh
+++ apktool/apktool-kali-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -L -o $PWD/apktool https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
-curl -L -o $PWD/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.6.1.jar
+curl -L -o $PWD/apktool.jar https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.7.0.jar
 sudo chmod +x apktool*  
 sudo mv apktool* /usr/bin/
